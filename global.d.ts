4dbf62eecd0ea91dcc89bd6686e4d07947180e9f
Przemysław Ratajczak <przemyslaw.ratajczak@shift4.com>
2025-07-23 09:33:12 +0200

    Removed type

--- global.d.ts
+++ global.d.ts
@@ -2,60 +2,10 @@ interface ApplePaySession {
   canMakePayments: () => boolean;
 }
 
-interface Shift4 {
-  createComponent: CreateComponent;
-  createComponentGroup: (options = {}) => Shift4ComponentGroup;
-  createToken: (componentOrToken: Shift4ComponentGroup | Shift4Component | PaymentResponse, tokenData: TokenRequest = {}) => Promise<Token>;
-  verifyThreeDSecure: (request: any) => Promise<Token>;
-  createPaymentRequest: (methodData: PaymentMethodData[], details: PaymentDetailsInit) => PaymentRequest;
-}
-
-type CreateComponent = (type: string, options = Record<string, any>) => Shift4Component;
-
-interface Shift4Component {
-  focus: () => void;
-  mount: (selector: string | HTMLElement) => void;
-  updateOptions: (options: Record<string, any>) => void;
-  clear: () => void;
-}
-
-interface Shift4ComponentGroup {
-  createComponent: CreateComponent;
-  automount: (selector: string | HTMLElement) => Shift4ComponentGroup;
-  updateComponentOptions: (type: string, options = Record<string, any>) =>  void;
-}
-
-interface Token {
-  tokenId: string;
-}
-
-interface TokenRequest {
-    googlePay?: {
-        token: string;
-    }
-    applePay?: {
-        token: object | string;
-    }
-    fraudCheckData?: {
-        phone?: string;
-        email?: string;
-        sessionId?: string;
-    }
-}
-
 declare global {
   namespace JSX {
     interface IntrinsicElements {
       'apple-pay-button': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-    }
-  }
-  namespace Shift4 {
-    interface Shift4 {
-      createComponent: CreateComponent;
-      createComponentGroup: (options?: Record<string, any>) => Shift4ComponentGroup;
-      createToken: (componentOrToken: Shift4ComponentGroup | Shift4Component | PaymentResponse, tokenData?: TokenRequest) => Promise<Token>;
-      verifyThreeDSecure: (request: any) => Promise<Token>;
-      createPaymentRequest: (methodData: PaymentMethodData[], details: PaymentDetailsInit) => PaymentRequest;
     }
   }
 }
