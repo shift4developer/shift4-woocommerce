a04bb88d07c4b77e8a2638de768a032441ceff0d
Przemysław Ratajczak <przemyslaw.ratajczak@shift4.com>
2025-08-26 08:36:22 +0200

    Refactor Shift4_WC_Validator: change RANDOM_PART_CHARS to an array and update token validation logic. Enhance autoloading for Utils namespace.

--- utils/Shift4_WC_Validator.php
+++ utils/Shift4_WC_Validator.php
@@ -6,7 +6,11 @@ namespace Shift4\WooCommerce\Utils;
 if (!defined('ABSPATH')) exit;
 
 class Shift4_WC_Validator {
-    const RANDOM_PART_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    const RANDOM_PART_CHARS = [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+    ];
     
     public static function isValidToken(string $token) : bool {
         if (strpos($token, 'tok_') !== 0) {
@@ -16,7 +20,7 @@ class Shift4_WC_Validator {
         $rest = substr($token, 4);
         $isValid = true;
         for ($i = 0; $i < strlen($rest); $i++) {
-            if (!in_array($rest[$i], self::RANDOM_PART_CHARS)) {
+            if (!in_array($rest[$i], self::RANDOM_PART_CHARS, true)) {
                 $isValid = false;
                 break;
             }
@@ -25,10 +29,10 @@ class Shift4_WC_Validator {
     }
 
     public static function validateToken(string $token): string {
-        $isValid = self::isValidCardToken($token);
+        $isValid = self::isValidToken($token);
 
         if (!$isValid) {
-            throw new Exception('Invalid card token format');
+            throw new \Exception('Invalid card token format');
         }
 
         return $token;
